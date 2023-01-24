afd0caefdb8e8e82ebfa50c74e87a692c4fac28a
Mntnbassfan <108299929+Mntnbassfan@users.noreply.github.com>
2023-01-23 23:40:54 -0700

    added base code and tabs

---
+++ 00-practice-app/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+            Having experience as a former professional musician performing as a
+            Tuba player in various symphony orchestras and theme parks and
+            freelance Bass player, I understand the need of online presence for
+            the promotion of the working professional musician and specialize in
+            the development of custom websites and online web applications to
+            assist these talented individuals with maximizing bookings and
+            greatly increasing their performance opportunities.
+      </p>
+    </div>
+  );
+}
---
+++ 00-practice-app/src/components/pages/Contact.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <div>
+          <ul>
+            <li>
+              <p><strong>Phone Number: (720)233-4070</strong></p>
+            </li>
+
+            <li>
+              <p><strong>Email: Hannatuba@yahoo.com</strong></p>
+            </li>
+
+            <li>
+              <a href="https://github.com/Mntnbassfan">
+                Connect to my GitHub</a
+              >
+            </li>
+
+            <li>
+              <a href="https://www.linkedin.com/feed/">
+                Connect to my LinkedIn</a
+              >
+            </li>
+          </ul>
+        </div>
+    </div>
+  );
+}
